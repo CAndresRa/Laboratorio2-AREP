2ff082551a618010a23b30704f53ab9a897e6de9
candresra <carlos.ramirez-t@mail.escuelaing.edu.co>
2020-08-14 17:24:41 -0500

    :rocket:- deploy

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -7,6 +7,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main( String[] args ) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
     }
 
